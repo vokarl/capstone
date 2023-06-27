1962c5572cc64d77a49dd3679ddd8a2f0fd48b23
Karl Vogl <karl.vogl@gmx.de>
2023-06-27 15:57:53 +0200

    added tree

--- resources/treeArray.js
+++ resources/treeArray.js
@@ -3,4 +3,5 @@ export const allTrees = [
   { id: 1, name: "Buchenstammholz" },
   { id: 2, name: "Douglasienstammholz" },
   { id: 3, name: "Fichten-/Tannestammholz" },
+  { id: 4, name: "Kiefer" },
 ];
