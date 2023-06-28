a6f5ab26fc04489d48e06c4c3bda05bb33db72bd
Karl Vogl <karl.vogl@gmx.de>
2023-06-28 08:34:28 +0200

    e corrections

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import TreeList from "../components/trees";
+import TreeList from "../components/TreeList";
 
 export default function HomePage() {
   return (
