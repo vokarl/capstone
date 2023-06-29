3df3d4ee431ec66e6d9ad6950998fc7b9b47ec44
Karl Vogl <karl.vogl@gmx.de>
2023-06-29 15:12:56 +0200

    vercel fix

--- components/TreeList.js
+++ components/TreeList.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { allTrees } from "../resources/TreeArray";
+import { allTrees } from "../resources/treearray";
 
 const TreeList = () => {
   return (
