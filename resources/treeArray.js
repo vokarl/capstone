61ce7281f77b00781b67198da1fd8c38f27184db
Karl Vogl <karl.vogl@gmx.de>
2023-06-29 15:00:30 +0200

    name correction

--- resources/treeArray.js
+++ resources/treeArray.js
@@ -3,5 +3,5 @@ export const allTrees = [
   { id: 1, name: "Buchenstammholz" },
   { id: 2, name: "Douglasienstammholz" },
   { id: 3, name: "Fichten-/Tannestammholz" },
-  { id: 4, name: "Kiefer" },
+  { id: 4, name: "Kiefernstammholz" },
 ];
