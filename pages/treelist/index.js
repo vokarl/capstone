45fbbe3293123eb53005870cd45d6d71943adf5d
Karl Vogl <karl.vogl@gmx.de>
2023-06-30 14:52:07 +0200

    array in index

--- pages/treelist/index.js
+++ pages/treelist/index.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
-import allTrees from "../../resources/treearray.js";
+const allTrees = [
+  { id: 0, name: "Eichenstammholz" },
+  { id: 1, name: "Buchenstammholz" },
+  { id: 2, name: "Douglasienstammholz" },
+  { id: 3, name: "Fichten-/Tannestammholz" },
+  { id: 4, name: "Kiefernstammholz" },
+];
 
 const TreeList = () => {
   return (
