6e4291971842e34ad05fe09c11884e9bb4c4c89f
Karl Vogl <karl.vogl@gmx.de>
2023-07-03 09:18:39 +0200

    vercel fix

--- pages/treelist/index.js
+++ pages/treelist/index.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import allTrees from "@/resources/treearray";
+import allTrees from "../../resources/treearray";
 
 const StyledList = styled.ul`
   list-style-type: disc;
@@ -10,8 +10,7 @@ const ListItem = styled.li`
   text-align: left;
   padding-left: 5rem;
   padding-top: 2rem;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+
   color: yellowgreen;
   background-color: rgb(54, 89, 0);
 `;
