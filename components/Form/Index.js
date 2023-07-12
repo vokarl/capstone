b0b8b25fd26c52d03bc105896e7055f543a58133
Karl Vogl <karl.vogl@gmx.de>
2023-07-12 22:35:47 +0200

    added tree sorting result list to sorting page

--- components/Form/Index.js
+++ components/Form/Index.js
@@ -3,8 +3,10 @@ import React from "react";
 let inputNumber = 1;
 
 const RenderInputField = ({ label }) => {
-  const inputId = `input$(inputNumber)`;
+  const inputId = `input${inputNumber}`;
   inputNumber++;
+  console.log("Input ID:", inputId);
+
   return (
     <>
       <label htmlFor={inputId}>{label}</label>
